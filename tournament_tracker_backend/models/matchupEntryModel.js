e4d76e96cd73a01b8f178172e02789237048ecc9
martinpur <m_pur@utb.cz>
2023-12-11 14:22:08 +0100

    Loading screens and sorting arrays

--- tournament_tracker_backend/models/matchupEntryModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-
-const matchupEntrySchema = mongoose.Schema({
-  teamCompetingId: String,
-  score: Number,
-  parentMatchupId: Number
-});
-
-export default mongoose.model("MatchupEntry", matchupEntrySchema);
--- tournament_tracker_backend/models/matchupModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-
-const matchupSchema = mongoose.Schema({
-  matchupRound: Number,
-  winnerTeamId: String
-});
-
-export default mongoose.model("Matchup", matchupSchema);
